44a7d89ee58c1d4822cac5a1ef523e3de73a1d64
Gabriel Costa <gpc3@icomp.ufam.edu.br>
2017-09-17 11:45:33 -0400

    Criação de método para inserção de palavras no documento

--- src/document.hpp
+++ src/document.hpp
@@ -4,4 +4,17 @@ struct Document {
     int id;
     int totalWordsCount;
     std::map<std::string, int> wordAmountOnDocument;
+
+public:
+    void addWord(const std::string& word) {
+        auto tuple = wordAmountOnDocument.find(word);
+
+        if(tuple == wordAmountOnDocument.end()) {
+            wordAmountOnDocument.emplace(word, 1);
+            totalWordsCount++;
+        }
+        else {
+            wordAmountOnDocument[word]++;
+        }
+    }
 };
