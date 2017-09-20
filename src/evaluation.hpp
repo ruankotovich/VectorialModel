4f8a514f3ed32ecb5b7f65b070770119e834f5e6
Gabriel Costa <gpc3@icomp.ufam.edu.br>
2017-09-20 18:49:31 -0400

    Avaliador pArroba

--- src/evaluation.hpp
+++ src/evaluation.hpp
@@ -41,7 +41,29 @@ double precisionR(Query query,
 }
 
 
-double precisionR(Query query, std::priority_queue<std::pair<int, double>, std::vector<std::pair<int, double>>, QueryComparator_t> documents) { 
-    return 0;
+double pArroba(Query query, std::priority_queue<std::pair<int, double>, std::vector<std::pair<int, double>>, QueryComparator_t> documents, double K) { 
+    if(K > documents.size()) {
+        return 0;
+    }
+    
+    std::set<int> relevantDocuments;
+    while (!query.docsByRelevance.empty()) {
+        relevantDocuments.insert(std::move(const_cast<int&>(query.docsByRelevance.top().first)));
+        query.docsByRelevance.pop();
+    }
+
+    int relevantsFound = 0;
+    std::pair<int, double> currentDoc;
+    for(int i = 0; i < K; ++i) {
+        currentDoc = documents.top();
+
+        if(relevantDocuments.find(currentDoc.first) != relevantDocuments.end()) {
+            ++relevantsFound;
+        }
+
+        documents.pop();
+    }
+
+    return relevantsFound/K;
 }
 #endif
